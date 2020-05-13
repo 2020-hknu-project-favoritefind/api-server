1bbc11706981b39dee4d2016266fe20afb8eca7e
vallisneria <moogi3469@gmail.com>
2020-05-14 00:24:15 +0900

    query string으로 db 검색할 수 있도록 변경

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,40 @@ router.get('/', function (req, res, next) {
 
 router.get("/place", (req, res, next) => {
   let cl = new Client(pw);
-  cl.connect();
-  cl.query("SELECT * FROM place;", (err, re) => {
-    res.json(re["rows"]);
-    cl.end();
+  let t = {
+    place_id: req.query.id,
+    city: req.query.city,
+    category: req.query.category,
+    subdivision: req.query.subdivision
+  }
+  let q = [];
+  Object.keys(t).forEach(i => {
+    if (t[i] !== undefined) {
+      if (i === 'place_id') {
+        q.push(`${i}=${t[i]}`)
+      } else {
+        q.push(`${i}='${t[i]}'`)
+      }
+    }
   })
+  cl.connect();
+
+  if (q) {
+    cl.query(`SELECT * FROM place WHERE ${q.join(" and ")};`, (err, re) => {
+      if (re.rows) {
+        res.json(re["rows"]);
+      }
+
+      cl.end();
+    })
+  } else {
+    cl.query("SELECT * FROM place;", (err, re) => {
+      res.json(re["rows"]);
+      cl.end();
+    })
+  }
 });
+
 
 router.get("/place/id/:id", (req, res, next) => {
   let cl = new Client(pw);
@@ -36,10 +64,10 @@ router.get("/place/city/:city", (req, res, next) => {
   })
 })
 
-router.get("/place/type/:type", (req, res, next) => {
+router.get("/place/category/:type", (req, res, next) => {
   let cl = new Client(pw);
   cl.connect();
-  cl.query(`SELECT * FROM place WHERE hobby_type='${decodeURI(req.params.type)}';`, (err, re) => {
+  cl.query(`SELECT * FROM place WHERE category='${decodeURI(req.params.type)}';`, (err, re) => {
     res.json(re["rows"]);
     cl.end();
   })
